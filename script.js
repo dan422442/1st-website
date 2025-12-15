917025bad3e6c5e518bd91b82ff09a5ac979d266
dan422442 <113399665+dan422442@users.noreply.github.com>
2025-12-15 15:11:47 +0000

    added stuff

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let c = 0
-
+let c = "";
+const p = document.createElement("p");
+p.textContent = `${c}`;
+const body = document.querySelector("body");
+body.appendChild(p);
 function para() {
-    c++;
-    const p = document.createElement("p");
-    p.textContent = `${c}`;
-    const body = document.querySelector("body");
-    body.appendChild(p);
+    c=prompt("text?");
+    if(c!=null)
+        p.textContent = `${c}`;
 }
 
 const b = document.querySelector("button");
 b.addEventListener("click", () => {
-    para()
+    para();
 })
