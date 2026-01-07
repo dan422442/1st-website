96d8a137ef9035fdc8dc5b32b428269d02ecf22f
dan422442 <113399665+dan422442@users.noreply.github.com>
2026-01-07 00:54:41 -0500

    Delete 0351.py

--- 0351.py
+++
@@ -1,9 +0,0 @@
-from functools import cache
-@cache
-def f(n):
-    if n==0:
-        return 0
-    return n*n-sum([f(n//i) for i in range(2,n+1)])
-def g(n):
-    return 3*(n*n-f(n)+n-1)
-print(g(100000000))
